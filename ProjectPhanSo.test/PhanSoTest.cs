c11f11bcfc4dc762609383271f68f19b9e41bdcd
trannguyenkhanh <tmkhanh69@gmail.com>
2015-12-03 11:03:14 +0700

    first commit

---
+++ ProjectPhanSo.test/PhanSoTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectPhanSo;
+using NUnit.Framework;
+
+namespace ProjectPhanSo.test
+{
+    [TestFixture]
+    public class PhanSoTest
+    {
+        [Test]
+        public void TestHamDungMacDinh()
+        {
+            PhanSo p;
+            p = new PhanSo();
+            Assert.AreEqual(0, p.Tu, "Loi o tu so");
+            Assert.AreEqual(1, p.Mau, "Loi o mau so");
+        }
+        [Test]
+        public void TestHamDung2ThamSo()
+        {
+            PhanSo p;
+            p = new PhanSo(3,4);
+            Assert.AreEqual(3, p.Tu, "Loi o tu so");
+            Assert.AreEqual(4, p.Mau, "Loi o mau so");
+        }
+        [Test]
+        public void Test_CongPSDuong()
+        {
+            PhanSo p1 = new PhanSo(1, 2);
+            PhanSo p2 = new PhanSo(3, 4);
+            PhanSo p3 = p1.Cong(p2);
+            Assert.AreEqual(10, p3.Tu, "Loi o tu so");
+            Assert.AreEqual(8, p3.Mau, "Loi o tu so");
+          
+        }
+    }
+}
---
+++ ProjectPhanSo/PhanSo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPhanSo
+{
+    public class PhanSo
+    {
+        private int tu;
+        public int Tu
+        {
+            get { return tu; }
+            set { tu = value; }
+        }
+        private int mau;
+        public int Mau
+        {
+            get { return mau; }
+            set { tu = value; }
+        }
+        public PhanSo()
+        {
+            tu = 0;
+            mau = 1;
+        }
+        public PhanSo(int x, int y)
+        {
+            tu = x;
+            mau = y;
+        }
+        public PhanSo Cong(PhanSo p)
+        {
+            PhanSo kq;
+            kq = new PhanSo();
+            kq.tu = tu * p.mau + mau * p.tu;
+            kq.mau = mau * p.mau;
+            return kq;
+        }
+        public override string ToString()
+        {
+            return tu + "/" + mau;
+        }
+
+     
+    }
+}
